82a33098605fe51e7ebf92ef495cbda646ecb76c
chufangao <gaoandy1445@gmail.com>
2024-06-10 03:20:37 -0500

    news headlines scraping

--- news_headlines/get_news.py
+++ news_headlines/get_news.py
@@ -1,15 +1,14 @@
 import os
-os.environ["HF_HOME"] = "/srv/local/data/chufan2/huggingface/"
+# os.environ["HF_HOME"] = "/srv/local/data/chufan2/huggingface/"
 import sys
+import os
 from tqdm.auto import tqdm, trange
 from datetime import datetime, timedelta
 import time
-import os
 import pandas as pd
 import numpy as np
-import pickle
 import json
-# import datetime
+import argparse
 import random
 from transformers import pipeline
 from sentence_transformers import SentenceTransformer, CrossEncoder
@@ -19,7 +18,14 @@ from gnews import GNews
 
 #convert to datetime
 def convert_to_datetime(date_str):
-# Mon, 20 May 1996 07:00:00 GMT
+    """ Convert a date string to a datetime object.
+        Only works for specific type returned by the API:
+        I.e. Mon, 20 May 1996 07:00:00 GMT to %a, %d %b %Y %H:%M:%S %Z
+
+        date_str: str, date string
+
+        Returns: datetime object or pd.NA
+    """
     try:
         return datetime.strptime(date_str, '%a, %d %b %Y %H:%M:%S %Z')
     except:
@@ -27,6 +33,14 @@ def convert_to_datetime(date_str):
     
 # weeks = 13*20
 def get_date_at_month(start_date, month_to_add):
+    """
+    Get the date at month_to_add months after start_date
+
+    start_date: tuple of (year, month, day)
+    month_to_add: int
+
+    Returns: tuple of (year, month, day), day is always 1, since we are interested in month
+    """
     start_month_ = start_date[1] + month_to_add
     years = start_month_ // 12
     month = start_month_ % 12
@@ -36,6 +50,16 @@ def get_date_at_month(start_date, month_to_add):
     return (start_date[0] + years, month, start_date[2])
 
 def get_related_news(keyword, start_date, log_dir, num_months=240):
+    """
+    Get news related to a keyword for num_months. Log the news to a json file.
+
+    keyword: str, industry sponsor to search for
+    start_date: tuple of (year, month, day)
+    log_dir: str, directory to save the news
+    num_months: int, number of months to get news for from start_date
+
+    Returns: dict, news for each month
+    """
     google_news = GNews()
     all_results = {}
     for i in range(num_months):
@@ -50,7 +74,7 @@ def get_related_news(keyword, start_date, log_dir, num_months=240):
         # print(google_news.start_date, google_news.end_date)
         # Get the news results
         results = google_news.get_news(keyword)
-        # random sleep to avoid getting blocked
+        # random sleep to avoid getting blocked, can be adjusted, but this works for me
         time.sleep(random.randint(1, 5)) # time.sleep(1)
         lens = len(results)
         print(f'Got {lens} news for {keyword} in {google_news.start_date} to {google_news.end_date}')
@@ -58,70 +82,55 @@ def get_related_news(keyword, start_date, log_dir, num_months=240):
         with open(log_dir+'news.json', "w") as f:
             json.dump(all_results, f)
         # dump the results
-
     # sorted_results= sorted(results, key=lambda x: datetime.strptime(x['published date'], "%a, %d %b %Y %H:%M:%S %Z"), reverse=True)
-
     return all_results
 
+def get_top_sponsors(sponsors, studies):
+    """
+    Get the top 1000 most popular phase 3 industry sponsors
+    
+    sponsors: pd.DataFrame, sponsors.txt
+    studies: pd.DataFrame, studies.txt
+
+    Returns: pd.DataFrame, top 1000 most popular phase 3 industry sponsors
+    """
+    # sponsors = pd.read_csv(data_path + './CITT/sponsors.txt', sep='|')
+    # studies = pd.read_csv(data_path + './CITT/studies.txt', sep='|', low_memory=False)
+    studies['study_first_submitted_date'] = pd.to_datetime(studies['study_first_submitted_date'])
+    sponsors = pd.merge(sponsors, studies[['nct_id', 'phase', 'study_first_submitted_date']], on='nct_id', how='left')
+    sponsors = sponsors[sponsors['agency_class']=='INDUSTRY']
+    sponsors.dropna(inplace=True)
+    sponsors = sponsors[sponsors['phase'].str.contains('Phase 3')]
+    top_sponsors = sponsors['name'].value_counts().head(1000)
+    # coverage_ = top_sponsors.sum() / sponsors['name'].value_counts().sum()
+    # print(coverage_) # 0.8548555767913166
+    combined = pd.merge(top_sponsors.reset_index(),
+                        sponsors.groupby('name')['study_first_submitted_date'].min().reset_index(),
+                        on='name', how='left')
+    return combined
+
 if __name__ == '__main__':
-    # google_news = GNews()
-    # pakistan_news = google_news.get_news('Pakistan')
-    # print(pakistan_news[0])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, default='get_news', help='get_news, process_news, correspond_news_and_studies')
+    args = parser.parse_args()
+    assert args.mode in ['get_news', 'process_news', 'correspond_news_and_studies']
 
-    # mode = 'get_news' 
-    # mode = 'process_news'
-    mode = 'correspond_news_and_studies'
-    print(f'Mode: {mode}')
+    print(f'args.mode: {args.mode}')
 
-    if mode == 'get_news':
-        # stock_price_df = pd.read_csv("./stock_prices_635_industries.csv").drop(columns=['Unnamed: 0'])
-        # stock_price_df['Date'] = pd.to_datetime(stock_price_df['Date'], format='%Y-%m-%d')
-        # ticker_dict = pickle.load(open("./ticker_dict_211.pkl", "rb"))
-        # filtered_ticker_dict = pickle.load(open("./filtered_ticker_dict_642.pkl", "rb"))
-        # keys = list(filtered_ticker_dict.keys())
-        # for f in keys:
-        #     if (f in ticker_dict.keys()) or (filtered_ticker_dict[f] not in stock_price_df['Ticker'].unique()):
-        #         del filtered_ticker_dict[f]
-                
-        # log_dir = './stock_news_logs/'
-        # global_i = 0
-        # for company, ticker in tqdm(filtered_ticker_dict.items()):
+    data_path = './CITT/'
+    log_dir = './news_logs/'
+    sponsors = pd.read_csv(data_path + 'sponsors.txt', sep='|')
+    studies = pd.read_csv(data_path + 'studies.txt', sep='|', low_memory=False)
+    combined = get_top_sponsors(sponsors, studies)
 
-        #     min_date = stock_price_df[stock_price_df['Ticker'] == ticker]['Date'].min()
-        #     start_date = (int(min_date.year), int(min_date.month), 1)
+    cache_folder = '/srv/local/data/chufan2/huggingface/'
+    sentiment_pipe = pipeline("text-classification", model="yiyanghkust/finbert-tone", device='cuda')
+    encoder = SentenceTransformer('microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext', cache_folder=cache_folder)
+    crossencoder = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-12-v2", max_length=512)
 
-        #     os.makedirs(log_dir + ticker, exist_ok=True)
-        #     news = get_related_news(company, start_date, num_months=12*50, log_dir=log_dir + ticker + '/')
-        #     global_i += 1
-        #     # break
-
+    if args.mode == 'get_news': # warning: this will take a long time (multiple weeks)
         # get top 1000 most popular phase 3 industry sponsors
-        data_path = './CITT/'
-        sponsors = pd.read_csv(data_path + 'sponsors.txt', sep='|')
-        studies = pd.read_csv(data_path + 'studies.txt', sep='|', low_memory=False)
 
-        studies['study_first_submitted_date'] = pd.to_datetime(studies['study_first_submitted_date'])
-        sponsors = pd.merge(sponsors, studies[['nct_id', 'phase', 'study_first_submitted_date']], on='nct_id', how='left')
-        sponsors = sponsors[sponsors['agency_class']=='INDUSTRY']
-        sponsors.dropna(inplace=True)
-        sponsors = sponsors[sponsors['phase'].str.contains('Phase 3')]
-
-        top_sponsors = sponsors['name'].value_counts().head(1000)
-        coverage_ = top_sponsors.sum() / sponsors['name'].value_counts().sum()
-        combined = pd.merge(top_sponsors.reset_index(),
-                            sponsors.groupby('name')['study_first_submitted_date'].min().reset_index(),
-                            on='name', how='left')
-
-        with open('./filtered_ticker_dict_642.pkl', 'rb') as f:
-            ticker_dict = pickle.load(f)
-        ticker_df = pd.DataFrame(ticker_dict.items(), columns=['name', 'ticker'])
-        combined = pd.merge(combined, ticker_df, on='name', how='left')
-        # keep only na
-        combined = combined[combined['ticker'].isna()]
-        combined.sort_values('name', inplace=True)
-        print(combined.shape, combined['name'])
-
-        log_dir = './stock_news_logs/_names/'
         global_i = 0
         for name in tqdm(sorted(combined['name'])):
             if name.lower() in os.listdir(log_dir):
@@ -131,30 +140,21 @@ if __name__ == '__main__':
                 min_date = combined[combined['name']==name]['study_first_submitted_date'].min()
                 # print(date.year, date.month, date.day)
                 start_date = (int(min_date.year), int(min_date.month), 1)
-
                 os.makedirs(log_dir+ name.lower(), exist_ok=True)            
-                news = get_related_news(name, start_date, num_months=12*50, log_dir=log_dir + name.lower() + '/')
+                news = get_related_news(name, start_date, num_months=12*50, log_path=log_dir + name.lower() + '.json')
             global_i += 1
             # break
 
     # ======================== Process the news data ========================
-    elif mode == 'process_news':
+    elif args.mode == 'process_news':
         print('Processing news data')
-        studies_df = pd.read_csv('./CITT/studies.txt', sep='|', low_memory=False)        
-        ticker_dict_df = pd.read_csv('stock_price/ticker_dict_642.csv')
-        ticker_dict = {row['name'].lower(): row['ticker'] for _, row in ticker_dict_df.iterrows()}
+        # studies_df = pd.read_csv('./CITT/studies.txt', sep='|', low_memory=False)        
+        # ticker_dict_df = pd.read_csv('stock_price/ticker_dict_642.csv')
+        # ticker_dict = {row['name'].lower(): row['ticker'] for _, row in ticker_dict_df.iterrows()}
 
         all_company_dfs = []
-        # for company, ticker in ticker_dict.items():
-        #     if not os.path.exists(os.path.join('./stock_news_logs/_names/', ticker, 'news.json')):     # if exists
-        #         continue
-        #     with open(os.path.join('./stock_news_logs/_names/', ticker, 'news.json'), 'rb') as f:
-        #         news = json.load(f)
-        for company in sorted(os.listdir('./stock_news_logs/_names/')):
-            if not os.path.exists(os.path.join('./stock_news_logs/_names/', company, 'news.json')):     # if exists
-                print(f'{company} does not have news')
-                continue
-            with open(os.path.join('./stock_news_logs/_names/', company, 'news.json'), 'rb') as f:
+        for company in sorted(os.listdir(log_dir)):
+            with open(os.path.join(company), 'rb') as f:
                 news = json.load(f)
             # print(company, ticker, news)
 
@@ -174,25 +174,23 @@ if __name__ == '__main__':
                         all_publishers.append(news[k][i]['publisher']['title'])
 
             df = pd.DataFrame({'date': all_dates, 'title': all_titles, 'description': all_descriptions, 'publisher': all_publishers})
-            if company in ticker_dict.keys():
-                ticker = ticker_dict[company]
-            else:
-                ticker = pd.NA
-            df['ticker'] = ticker
+            # if company in ticker_dict.keys():
+            #     ticker = ticker_dict[company]
+            # else:
+            #     ticker = pd.NA
+            # df['ticker'] = ticker
             df['company'] = company
             all_company_dfs.append(df)
         all_company_dfs = pd.concat(all_company_dfs)
         # all_company_dfs.to_csv('./stock_price/news_tmp.csv', index=False); quit() # update old news.csv with company names
 
         print("Processing sentiment")
-        pipe = pipeline("text-classification", model="yiyanghkust/finbert-tone", device='cuda')
         batch_size = 512
-
         all_label_preds = []
         all_label_probs = []
         titles = all_company_dfs['title'].tolist()
         for i in trange(0, len(titles), batch_size):
-            out = pipe(titles[i:i+batch_size], batch_size=batch_size)
+            out = sentiment_pipe(titles[i:i+batch_size], batch_size=batch_size)
             all_label_preds += [o['label'] for o in out]
             all_label_probs += [o['score'] for o in out]
         all_company_dfs['sentiment'] = all_label_preds
@@ -200,35 +198,22 @@ if __name__ == '__main__':
 
         # process title embeddings using pubmedbert
         print("Processing embeddings")
-        encoder = SentenceTransformer('microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext', cache_folder='/srv/local/data/chufan2/huggingface/')
         encoded_titles = encoder.encode(titles, convert_to_numpy=True, device='cuda', batch_size=batch_size)
-        np.save('./stock_price/news_title_embeddings.npy', encoded_titles)
+        np.save('./news_title_embeddings.npy', encoded_titles)
 
-        encoded_studies = encoder.encode(studies_df['brief_title'].tolist(), convert_to_numpy=True, device='cuda', batch_size=batch_size)
-        np.save('./stock_price/studies_title_embeddings.npy', encoded_studies)
+        # encoded_studies = encoder.encode(studies['brief_title'].tolist(), convert_to_numpy=True, device='cuda', batch_size=batch_size)
+        # np.save('./studies_title_embeddings.npy', encoded_studies)
 
-        all_company_dfs.to_csv('./stock_price/news.csv', index=False)
+        all_company_dfs.to_csv('./news.csv', index=False)
 
-        # crossencoder = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-12-v2", max_length=512, cache_folder='/srv/local/data/chufan2/huggingface/')
-        # scores = crossencoder.predict(
-        #     [("Query", "Paragraph1"), ("Query", "Paragraph2"), ("Query", "Paragraph3")]
-        # )
-    elif mode == 'correspond_news_and_studies':
-        crossencoder = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-12-v2", max_length=512)
-        encoder = SentenceTransformer('microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext', cache_folder='/srv/local/data/chufan2/huggingface/')
+    elif args.mode == 'correspond_news_and_studies':
+        news_df = pd.read_csv('./news.csv')
+        news_title_embedding = np.load('./news_title_embeddings.npy')
+        top_sponsors = combined
+        interventions = pd.read_csv(data_path+'interventions.txt', sep='|')
+        conditions = pd.read_csv(data_path+'conditions.txt', sep='|')
 
-        news_df = pd.read_csv('./stock_price/news.csv')
-        news_title_embedding = np.load('./stock_price/news_title_embeddings.npy')
-        studies = pd.read_csv('./CITT/studies.txt', sep='|', low_memory=False)
-        sponsors = pd.read_csv('./CITT/sponsors.txt', sep='|')
-        top_sponsors = pd.read_csv('stock_price/top_sponsors.csv')
-        interventions = pd.read_csv('./CITT/interventions.txt', sep='|')
-        conditions = pd.read_csv('./CITT/conditions.txt', sep='|')
-
-        sponsors = sponsors[sponsors['name'].isin(top_sponsors['name'])]
-        sponsors['name'] = sponsors['name'].str.lower()
-
-        studies = studies[studies['nct_id'].isin(sponsors['nct_id'])]
+        studies = studies[studies['nct_id'].isin(top_sponsors['nct_id'])]
         studies = studies[studies['nct_id'].isin(interventions['nct_id'])]
         studies = studies[studies['nct_id'].isin(conditions['nct_id'])]
         news_df['date'] = pd.to_datetime(news_df['date'])
@@ -247,7 +232,7 @@ if __name__ == '__main__':
         studies['title2'] = studies['intervention_name'] + ' ' + studies['condition_name']
 
         studies_title2_embedding = encoder.encode(studies['title2'], convert_to_numpy=True, device='cuda', show_progress_bar=True)
-        np.save('./stock_price/studies_title2_embeddings.npy', studies_title2_embedding)
+        np.save('./studies_title2_embeddings.npy', studies_title2_embedding)
 
         # print(news_df.shape, news_title_embedding.shape, studies.shape, studies_title2_embedding.shape)
         # # most relevant news for each study
@@ -262,7 +247,7 @@ if __name__ == '__main__':
             if studies.iloc[i]['completion_date'] is pd.NaT:
                 continue
             # get sponsors of the study
-            sponsors_ = sponsors[sponsors['nct_id'] == studies.iloc[i]['nct_id']]['name'].tolist()
+            sponsors_ = top_sponsors[top_sponsors['nct_id'] == studies.iloc[i]['nct_id']]['name'].tolist()
             news_df_ = news_df[news_df['company'].isin(sponsors_)]
             news_df_ = news_df_[
                 np.abs((news_df_['date'] - studies.iloc[i]['completion_date']).dt.days) < 365*2
@@ -280,4 +265,4 @@ if __name__ == '__main__':
             studies.iloc[i, column_ind:column_ind+len(inds)] = news_df_.iloc[inds].index
             studies.iloc[i, column_ind+top_k:column_ind+top_k+len(news_df_)] = sims
 
-        studies.to_csv('./stock_price/studies_with_news.csv', index=False)
+        studies.to_csv('./studies_with_news.csv', index=False)
